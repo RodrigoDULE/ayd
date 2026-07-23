71a9f6285833ee3754c03a90ec176a2d6e78a00f
Leo <legonardo2099@gmail.com>
2026-07-23 11:48:14 -0600

    empezando servicio de calendario

---
+++ src/main/java/mx/uam/ayd/proyecto/negocio/servicioCalendario.java
@@ -0,0 +1,15 @@
+package mx.uam.ayd.proyecto.negocio;
+
+import org.springframework.stereotype.Service;
+import mx.uam.ayd.proyecto.negocio.EntidadNegocio.Evento;
+import mx.uam.ayd.proyecto.datos.RepositorioEvento;
+import mx.uam.ayd.proyecto.negocio.EntidadNegocio.Empleado;
+import mx.uam.ayd.proyecto.datos.RepositorioEmpleado;
+
+
+
+
+@Service
+public class servicioCalendario {
+    
+}
