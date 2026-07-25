75c46f4e1adb99a30b1249f36d7fc9caf58eb0dc
Leo <legonardo2099@gmail.com>
2026-07-24 19:31:09 -0600

    agregados metodos y cambios en repo evento para listas

--- src/main/java/mx/uam/ayd/proyecto/datos/RepositorioEvento.java
+++ src/main/java/mx/uam/ayd/proyecto/datos/RepositorioEvento.java
@@ -1,6 +1,7 @@
 package mx.uam.ayd.proyecto.datos;
 
 import java.time.LocalDate;
+import java.util.List;
 
 import org.springframework.data.repository.CrudRepository;
 import mx.uam.ayd.proyecto.negocio.EntidadNegocio.Evento;
@@ -8,6 +9,7 @@ import mx.uam.ayd.proyecto.negocio.EntidadNegocio.Evento;
 public interface RepositorioEvento extends CrudRepository<Evento, Long> {
 
     public Evento findByIdEvento(long idEvento);
-    public Evento findByFechaE(LocalDate fechaE);
+    public List <Evento> findByFechaE(LocalDate fechaE);
     public Evento findByNotificacion(LocalDate notificacion);
+    
 }
--- src/main/java/mx/uam/ayd/proyecto/negocio/servicioCalendario.java
+++ src/main/java/mx/uam/ayd/proyecto/negocio/servicioCalendario.java
@@ -1,8 +1,13 @@
 package mx.uam.ayd.proyecto.negocio;
+import java.time.LocalDate;
+import java.util.List;
 
+//leo D
 import org.springframework.stereotype.Service;
 import mx.uam.ayd.proyecto.negocio.EntidadNegocio.Evento;
 import mx.uam.ayd.proyecto.datos.RepositorioEvento;
+import mx.uam.ayd.proyecto.datos.repositorioCliente;
+import mx.uam.ayd.proyecto.negocio.EntidadNegocio.Cliente;
 import mx.uam.ayd.proyecto.negocio.EntidadNegocio.Empleado;
 import mx.uam.ayd.proyecto.datos.RepositorioEmpleado;
 
@@ -11,9 +16,26 @@ import mx.uam.ayd.proyecto.datos.RepositorioEmpleado;
 
 @Service
 public class servicioCalendario {
-    
-    
 
-    //metodo para recuperar eventos por fecha
-    //public Recupera 
+    //constructor 
+    private RepositorioEvento repoEvento;
+    public servicioCalendario(RepositorioEvento repoEvento){
+        this.repoEvento = repoEvento;
+    }
+
+
+
+    //metodo para recuperar eventos por fecha (del evento)
+    
+    public List<Evento> recuperaEventoporFecha(LocalDate fechaE){
+        return (List<Evento>) repoEvento.findByFechaE(fechaE);
+    }
+    
+    
+    //recuperar eventos por notificacion
+    public Evento recuperaPorNotificacion(LocalDate notificacion){
+        return repoEvento.findByNotificacion(notificacion);
+    }
+
 }
+
