be3742217b3b92cf4ee68c3e27bef318ca6ee3fa
Leo <legonardo2099@gmail.com>
2026-07-26 14:56:00 -0600

    afrefadas cardinalidades entre publicacuion y marketing

--- src/main/java/mx/uam/ayd/proyecto/datos/RepositorioFormularioMarketing.java
+++ src/main/java/mx/uam/ayd/proyecto/datos/RepositorioFormularioMarketing.java
@@ -1,7 +1,11 @@
 package mx.uam.ayd.proyecto.datos;
 import org.springframework.data.repository.CrudRepository;
+
+
 import mx.uam.ayd.proyecto.negocio.EntidadNegocio.FormularioMarketing;
 
 public interface RepositorioFormularioMarketing extends CrudRepository<FormularioMarketing, Long> {
+
+    public FormularioMarketing findById(long idFormulario);
 
 }
