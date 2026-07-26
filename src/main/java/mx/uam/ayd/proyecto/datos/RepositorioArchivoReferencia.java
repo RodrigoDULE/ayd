30f3b72021697df1a99317b57d0e5a16d15a9ceb
Valeria <valeria.danai20@gmail.com>
2026-07-26 13:46:32 -0600

    Moificacion repositorio archivo de referencia

--- src/main/java/mx/uam/ayd/proyecto/datos/RepositorioArchivoReferencia.java
+++ src/main/java/mx/uam/ayd/proyecto/datos/RepositorioArchivoReferencia.java
@@ -1,16 +1,16 @@
-
-
 package mx.uam.ayd.proyecto.datos;
 
 import org.springframework.data.repository.CrudRepository;
 import mx.uam.ayd.proyecto.negocio.EntidadNegocio.ArchivoReferencia;
 import mx.uam.ayd.proyecto.negocio.EntidadNegocio.Evento;
 
+public interface RepositorioArchivoReferencia extends CrudRepository<ArchivoReferencia, Long> {
 
-public interface RepositorioArchivoReferencia extends CrudRepository{
+    // Spring buscará directamente la variable 'id'
+    public ArchivoReferencia findById(long id);
 
-    public ArchivoReferencia findByIdArchivoReferencia(long id);
-
-    public ArchivoReferencia findByNombArchivoReferencia(String nombre);
+    // Spring buscará directamente la variable 'nombre'. 
+    // (Asumiendo que la variable en la clase ArchivoReferencia se llama "nombre")
+    public ArchivoReferencia findByNombre(String nombre);
 
 }
