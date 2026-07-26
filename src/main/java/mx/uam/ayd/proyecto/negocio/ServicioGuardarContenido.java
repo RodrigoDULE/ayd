aba42368119f4b86d1a2f9d6a48d90087c24dbf3
Leo <legonardo2099@gmail.com>
2026-07-26 11:18:34 -0600

    fxml de HU10 agregados

---
+++ src/main/java/mx/uam/ayd/proyecto/negocio/ServicioGuardarContenido.java
@@ -0,0 +1,10 @@
+package mx.uam.ayd.proyecto.negocio;
+
+public class ServicioGuardarContenido {
+
+
+
+
+    
+
+}
---
+++ src/main/java/mx/uam/ayd/proyecto/presentacion/HU10GuardarContenido/VistaGuardarContenido.java
@@ -0,0 +1,5 @@
+package mx.uam.ayd.proyecto.presentacion.HU10GuardarContenido;
+
+public class VistaGuardarContenido {
+
+}
